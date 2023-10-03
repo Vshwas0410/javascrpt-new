dd871eb6c9a868a48a301ae85c8f800a4eff3821
Vshwas0410 <122030482+Vshwas0410@users.noreply.github.com>
2023-10-03 12:46:52 +0000

    new

---
+++ 01-basics/conversionOperation.js
@@ -0,0 +1,27 @@
+//let score=null
+ let score=33
+// let score="33ab"
+ console.log(typeof score);
+ //console.log(typeof (score));
+ let valueInNumber=Number(score)
+ console.log(typeof valueInNumber)
+ console.log(valueInNumber);
+ /*
+ 1- "33"=33
+ 2- "3ab"=NaN
+ 3- true=1;false=0;
+
+ */
+//let isLoogedIn=33 
+let isLoogedIn="33ab" 
+//let isLoogedIn="Hitesh"
+//or "3"/"hitesh"
+let booloeanIsLoggedIn=Boolean(isLoogedIn)
+console.log(booloeanIsLoggedIn);
+// if " "(blank space double enverted comma ) than value is false
+//if "Hitesh" than valueis true
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
