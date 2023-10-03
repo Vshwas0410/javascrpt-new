ba3351af2b188b0f09973054e3b86ac837efb5fa
Vshwas0410 <122030482+Vshwas0410@users.noreply.github.com>
2023-10-03 11:02:29 +0000

    learned-variable

---
+++ 01-basics/variables.js
@@ -0,0 +1,16 @@
+const accountId=14545
+let accountEmail="vishwas@gamol.com"
+var accountPassword="1254"
+var accountCity="Jaipur"
+let accountState;
+
+//accountId=147// not allowed
+
+console.log(accountId);
+accountEmail="vsih@google.com"
+accountPassword="555"
+accountCity="Lucknow"
+console.table([accountId,accountCity,accountEmail,accountPassword,accountState])
+
+
+/*Prefer not to use var because of issue in block scope and functional scope */
