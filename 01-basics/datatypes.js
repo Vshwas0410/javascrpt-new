f5f3235a8f6afe5394cbf2b7f8e54c68e4d1d7fa
Vshwas0410 <122030482+Vshwas0410@users.noreply.github.com>
2023-10-03 11:35:29 +0000

    learned datatypes

---
+++ 01-basics/datatypes.js
@@ -0,0 +1,17 @@
+"use strict";// treat all JS code as newer version 
+alert("hello")//we are using nodejs not browser
+console.log(3+3);console.log("hitesh")//code readiablity should be high
+let  age=18
+let isLoggedIn= false
+let state =null
+//number=2 to power 53
+//bigint
+// string=> " "
+//bollean=true/false
+//null= standalone value
+// undefined=>
+// symbol=>unique
+//object
+console.log(typeof "vishwas");// string
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
