44cde28a2b35ddc70b1569864ee664e3db1fe136
Vshwas0410 <122030482+Vshwas0410@users.noreply.github.com>
2023-10-05 11:09:38 +0000

    putt

---
+++ 01-basics/stack-heap.js
@@ -0,0 +1,14 @@
+let myYoutubename="myChannelVishwas";
+let anothername=myYoutubename;
+
+anothername="chaiaurcode"
+console.log(myYoutubename);
+console.log(anothername);
+let userOne={
+    email:"user@google.com",
+    upi:"user@ybl"
+}
+let userTwo=userOne;
+userTwo.email="vishwas@google.com"
+console.log(userOne.email);
+console.log(userTwo.email);
