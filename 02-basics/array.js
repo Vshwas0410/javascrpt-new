81857cb1a53b9ad2f647ac619bebdea46b21cc6f
Vshwas0410 <122030482+Vshwas0410@users.noreply.github.com>
2023-10-07 09:50:40 +0000

    arraycommit

---
+++ 02-basics/array.js
@@ -0,0 +1,28 @@
+const myArr=[0,11,2,3,4,5]
+// const myHeros=["shaktiman","naagraj"]
+// console.log(myArr[1]);
+// myArr.push(6)
+// console.log(myArr);
+// myArr.pop()
+// console.log(myArr);
+// myArr.unshift(9)
+// console.log(myArr);
+// myArr.shift()
+// // console.log(myArr);
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(988));
+// console.log(myArr.findIndex(5));
+// const newArr= myArr.join()// it will convert in string
+// console.log(myArr);
+// console.log(typeof newArr);
+console.log("A =",myArr);
+const myn1=myArr.slice(1,3)
+console.log("A =",myn1);
+console.log("B =",myArr);
+const myn2=myArr.splice(1,3)
+console.log("A =",myn1);
+console.log("B =",myn2);
+
+
+
+
