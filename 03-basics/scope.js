3e88bd3ec5e7afd4db59b2e04f753e5dcb4443d3
Vshwas0410 <122030482+Vshwas0410@users.noreply.github.com>
2023-10-08 10:46:37 +0000

    comment

--- 03-basics/scope.js
+++ 03-basics/scope.js
@@ -10,3 +10,46 @@ console.log("INNER: ",a);
 //console.log(a);
 //console.log(b);
 console.log(c);
+
+
+
+
+
+
+function one(){
+    const userName="vishwas"
+    function two() {
+        const website="youtube"
+console.log(userName);        
+    }
+    //console.log(website);
+    //two()
+}
+one()
+
+
+if(true){
+    const userName="Vishwas"
+if(userName==="Vishwas"){
+    const website= "youtube"
+    console.log("userName "+ website);
+}
+//console.log(website);
+}
+//console.log(userName);
+
+
+/************Interesting *********************/
+console.log(addOne(5))
+function addOne(num) {
+    return num+1
+    
+}
+addOne(5)
+
+
+
+const addTwo = function(num){
+    return num+2
+}
+addTwo(5)
